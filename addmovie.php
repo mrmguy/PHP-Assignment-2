e5fdffddda10da634f0534618579082f31e33c48
Mark Burnstein <mark.burnstein@gmail.com>
2015-02-12 16:14:49 -0800

    added form ability to add items to database

--- addmovie.php
+++ addmovie.php
@@ -1,6 +1,69 @@
 <?php
-
-error_reporting(E_ALL);
+  error_reporting(E_ALL);
   ini_set('display-errors', 'On');
-echo 'this is where add movie code goes';
+$mysqli = new mysqli("localhost", "root", "", "trialdb");
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+/* Prepared statement, stage 1: prepare */
+if (!($stmt = $mysqli->prepare("INSERT INTO video_store(name, category, length) VALUES (?, ?, ?)"))) {
+     echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+}
+
+//convert $_POST
+
+//check that all values exist
+
+  foreach ($_POST as $key => $value) {
+    if (empty($value)) {
+      echo "Missing parameter - " . $key;
+      echo '<br>';
+      $testNumber = False;
+    }
+  }
+
+  echo '<br>';
+
+//check if minutes valid value
+
+if (!ctype_digit($_POST['minutes'])) {
+      echo $key . " must be a length of minutes";
+      echo '<br>';
+  }
+  
+  echo '<br>';
+
+
+$name = $_POST['name'];
+$category = $_POST['category'];
+$minutes = $_POST['minutes'];
+
+
+/* Prepared statement, stage 2: bind and execute */
+
+if (!$stmt->bind_param("ssi", $name, $category, $minutes)) {
+    echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
+}
+
+if (!$stmt->execute()) {
+    echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+}
+
+// /* Prepared statement: repeated execution, only data transferred from client to server */
+// for ($id = 2; $id < 5; $id++) {
+//     if (!$stmt->execute()) {
+//         echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+//     }
+// }
+
+// /* explicit close recommended */
+// $stmt->close();
+
+// /* Non-prepared statement */
+// $res = $mysqli->query("SELECT id FROM test");
+// var_dump($res->fetch_all
+
+
+$mysqli->close();
+header('Location: moviedb.php');
 ?>
--- moviedb.php
+++ moviedb.php
@@ -14,9 +14,12 @@ $sqt = "CREATE TABLE video_store (
     rented bool NOT NULL DEFAULT 1
     );";
 
+if (!$mysqli->query('DESCRIBE video_store')) {
+
 /* Non-prepared statement */
-if (!$mysqli->query("DROP TABLE IF EXISTS video_store") || !$mysqli->query($sqt)) {
+if (!$mysqli->query($sqt)) {
     echo "Table creation failed: (" . $mysqli->errno . ") " . $mysqli->error;
+}
 }
 ?>
 
@@ -33,10 +36,10 @@ if (!$mysqli->query("DROP TABLE IF EXISTS video_store") || !$mysqli->query($sqt)
     <p><input type="text" name="name"></p>
 
     <p>Category</p>
-    <p><input type="text" name="name"></p>
+    <p><input type="text" name="category"></p>
     
     <p>Length:</p>
-    <p><input type="number" name="length"></p>
+    <p><input type="text" name="minutes"></p>
     <p><input type="submit" value = "Add Film"></p>
 </fieldset>
 
@@ -51,36 +54,36 @@ if (!$mysqli->query("DROP TABLE IF EXISTS video_store") || !$mysqli->query($sqt)
 </body>
 </html>
 <?php
-/* Prepared statement, stage 1: prepare */
-if (!($stmt = $mysqli->prepare("INSERT INTO video_store(name, category, length) VALUES (?, ?, ?)"))) {
-     echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
-}
-
-/* Prepared statement, stage 2: bind and execute */
-$name = 'test';
-$category = 'comedy';
-$length = 120;
-if (!$stmt->bind_param("ssi", $name, $category, $length)) {
-    echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
-}
-
-if (!$stmt->execute()) {
-    echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
-}
-
-// /* Prepared statement: repeated execution, only data transferred from client to server */
-// for ($id = 2; $id < 5; $id++) {
-//     if (!$stmt->execute()) {
-//         echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
-//     }
+// /* Prepared statement, stage 1: prepare */
+// if (!($stmt = $mysqli->prepare("INSERT INTO video_store(name, category, length) VALUES (?, ?, ?)"))) {
+//      echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
 // }
 
-// /* explicit close recommended */
-// $stmt->close();
+// /* Prepared statement, stage 2: bind and execute */
+// $name = 'test';
+// $category = 'comedy';
+// $length = 120;
+// if (!$stmt->bind_param("ssi", $name, $category, $length)) {
+//     echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
+// }
 
-// /* Non-prepared statement */
-// $res = $mysqli->query("SELECT id FROM test");
-// var_dump($res->fetch_all
+// if (!$stmt->execute()) {
+//     echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+// }
+
+// // /* Prepared statement: repeated execution, only data transferred from client to server */
+// // for ($id = 2; $id < 5; $id++) {
+// //     if (!$stmt->execute()) {
+// //         echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+// //     }
+// // }
+
+// // /* explicit close recommended */
+// // $stmt->close();
+
+// // /* Non-prepared statement */
+// // $res = $mysqli->query("SELECT id FROM test");
+// // var_dump($res->fetch_all
 
 
 $mysqli->close();
